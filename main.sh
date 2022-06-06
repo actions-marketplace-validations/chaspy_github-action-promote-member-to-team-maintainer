b8ef0b5f989f261be64b8449ece4975e723c3628
kondo takeshi <chaspy@users.noreply.github.com>
2022-06-06 15:47:30 +0900

    Support organization

--- main.sh
+++ main.sh
@@ -4,10 +4,11 @@ set -o pipefail
 
 USER=$1
 TEAM=$2
+ORG=$3
 
 gh api \
   --method PUT \
   -H "Accept: application/vnd.github.v3+json" \
-  "/orgs/quipper/teams/${TEAM}/memberships/${USER}" \
+  "/orgs/${ORG}/teams/${TEAM}/memberships/${USER}" \
   -f role='maintainer'
 
